8773785266de8bbcc3cd079d38a4333348205877
dylanbaghel <baghelabhi39@gmail.com>
2018-08-15 17:18:14 +0530

    Added Delete route DELETE /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
-        return res.status(404).send('Id Not Found');
+        return res.status(404).send('Invalid ID');
     }
 
     Todo.findById(id).then((todo) => {
@@ -56,6 +56,24 @@ app.get('/todos/:id', (req, res) => {
         res.status(200).send({todo});
     })
     .catch((e) => {
+        res.status(400).send(e);
+    });
+});
+
+//DELETE - ROUTE /todos/:id - REMOVE PARTICULAR TODO
+app.delete('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send('Invalid ID');
+    }
+
+    Todo.findByIdAndRemove(id).then((todo) => {
+        if(!todo) {
+            return res.status(404).send();
+        }
+        res.status(200).send({todo});
+    }).catch((e) => {
         res.status(400).send(e);
     });
 });
