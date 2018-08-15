0d52fd4668b3991ba153ca47eed40f6b1b8b9d4d
dylanbaghel <baghelabhi39@gmail.com>
2018-08-15 18:24:57 +0530

    Added PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const MONGO_URI = 'mongodb://dylan:anya2692@ds123012.mlab.com:23012/dylan-todo-api';
+const LOCAL_URI = 'mongodb://localhost:27017/TodoApp';
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {  useNewUrlParser: true});
+mongoose.connect(MONGO_URI, {  useNewUrlParser: true});
 
 module.exports = {mongoose};
