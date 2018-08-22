7583ccb5733d580ac22ffa86fbf88f68ad54d732
dylanbaghel <baghelabhi39@gmail.com>
2018-08-22 19:01:47 +0530

    Fixed Config

--- server/config/config.js
+++ server/config/config.js
@@ -3,9 +3,11 @@ console.log('env *******', env);
 if (env === 'development') {
     process.env.PORT = 3000;
     MONGO_URI = 'mongodb://localhost:27017/TodoApp';
+    process.env.JWT_SECRET = 'askjgkshjkghsags';
 } else if(env === 'test') {
     process.env.PORT = 3000;
     MONGO_URI = 'mongodb://localhost:27017/TodoAppTest';
+    process.env.JWT_SECRET = 'askjgkshjkghsagsjlksdjf';
 } else if(env === 'production') {
     MONGO_URI = 'mongodb://dylan:anya2692@ds123012.mlab.com:23012/dylan-todo-api';
 }
