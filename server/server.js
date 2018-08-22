64e9d1b7a8ee650e7a3aed926650a76fef7d13a8
dylanbaghel <baghelabhi39@gmail.com>
2018-08-22 18:31:12 +0530

    Associate Todos With Users

--- server/server.js
+++ server/server.js
@@ -22,9 +22,10 @@ app.use(express.json());
 //ROUTES
 
 //POST - ROUTE /todos
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
     let todo = new Todo({
-        text: req.body.text
+        text: req.body.text,
+        _creator: req.user._id
     });
 
     todo.save().then((todo) => {
@@ -36,8 +37,10 @@ app.post('/todos', (req, res) => {
 });
 
 //GET - ROUTE /todos - LIST ALL TODOS
-app.get('/todos', (req, res) => {
-    Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+    Todo.find({
+        _creator: req.user._id
+    }).then((todos) => {
         res.status(200).send({ todos });
     }).catch((e) => {
         res.status(400).send(e);
@@ -45,7 +48,7 @@ app.get('/todos', (req, res) => {
 });
 
 //GET - ROUTE /todos/:id - LIST INDIVIDUAL TODO
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
 
     const id = req.params.id;
 
@@ -53,7 +56,10 @@ app.get('/todos/:id', (req, res) => {
         return res.status(404).send('Invalid ID');
     }
 
-    Todo.findById(id).then((todo) => {
+    Todo.findOne({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -65,14 +71,17 @@ app.get('/todos/:id', (req, res) => {
 });
 
 //DELETE - ROUTE /todos/:id - REMOVE PARTICULAR TODO
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
     const id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send('Invalid ID');
     }
 
-    Todo.findByIdAndRemove(id).then((todo) => {
+    Todo.findOneAndRemove({
+        _id: id,
+        _creator: req.user._id
+    }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
@@ -83,7 +92,7 @@ app.delete('/todos/:id', (req, res) => {
 });
 
 //PATCH - ROUTE /todos/:id - UPDATE PARTICULAR TODO
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
     const id = req.params.id;
     const body = _.pick(req.body, ['text', 'completed']);
 
@@ -98,7 +107,10 @@ app.patch('/todos/:id', (req, res) => {
         body.completedAt = null;
     }
 
-    Todo.findByIdAndUpdate(id, { $set: body }, { new: true }).then((todo) => {
+    Todo.findOneAndUpdate({
+        _id: id,
+        _creator: req.user._id
+    }, { $set: body }, { new: true }).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
