190413e3e82839495a55e17069d69925b51539e1
dylanbaghel <baghelabhi39@gmail.com>
2018-08-22 16:29:34 +0530

    Hash User Password Before Saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,15 @@
-const { SHA256 } = require('crypto-js');
-const jwt = require('jsonwebtoken');
+// const { SHA256 } = require('crypto-js');
+// const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-    id: 10
-};
+// let data = {
+//     id: 10
+// };
 
-const token = jwt.sign(data, 'dylan');
-console.log(token);
-const decoded = jwt.verify(token, 'dylan');
-console.log(decoded);
+// const token = jwt.sign(data, 'dylan');
+// console.log(token);
+// const decoded = jwt.verify(token, 'dylan');
+// console.log(decoded);
 
 
 
@@ -37,3 +38,19 @@ console.log(decoded);
 // } else {
 //     console.log('Data was changed. Do not trust');
 // }
+
+// ===================================================================
+
+var password = 'dylan';
+
+// bcrypt.genSalt(10, (err, salt) => {
+//     bcrypt.hash(password, salt, (err, hash) => {
+//         console.log(hash);
+//     });
+// });
+
+var hashedPassword = '$2a$10$75pM1EmiRQUDL6I5JKBMZuo2FVRxaxRWF2TKDpBxcqdk5R5zpPpci';
+
+bcrypt.compare('dylanb', hashedPassword).then((res) => {
+    console.log(res);
+});
